c7d4d9f90564242df4bdb8222361784ccc658ec5
Frank <kipfrankline@gmail.com>
2020-07-05 22:52:08 +0300

    Add class to define source and articles

--- config.py
+++ config.py
@@ -1,29 +1,29 @@
 import os
 class Config:
-    '''
+    """
     General configuration parent class
-    '''
+    """
     NEWS_API_BASE_URL ='https://newsapi.org/v2/everything?q={}&apiKey={}'
     ARTICLES_API_BASE_URL  = 'https://newsapi.org/v2/articles?q={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('fdee0f42def74c7bad48a87e16fd6f08')
     SECRET_KEY = os.environ.get('1234')
 
 class ProdConfig(Config):
-    '''
+    """
     Pruduction  configuration child class
 
     Args:
         Config: The parent configuration class with General configuration settings
-    '''
+    """
         
     pass
     
 class DevConfig(Config):
-    '''
+    """
     Development configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
-    '''
+    """
     DEBUG = True
 config_options = {
  'development' : DevConfig,
